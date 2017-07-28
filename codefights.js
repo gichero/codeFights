e26f4ebf6aacd104eedb60637cd4636a72ff4e15
Amos <amosgathungu@gmail.com>
2017-07-28 19:27:38 -0400

    algorithm practice

--- codefights.js
+++ codefights.js
@@ -53,3 +53,31 @@ function shapeArea(n) {
         }
     }
     return count;
+
+//almostIncreasingSequence
+// Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
+//
+// Example
+//
+// For sequence = [1, 3, 2, 1], the output should be
+// almostIncreasingSequence(sequence) = false;
+
+
+    function almostIncreasingSequence(sequence) {
+
+    var count = 0;
+
+    for(var i = 0; i < sequence.length; i++){
+
+        if(sequence[i] <= sequence[i-1]){
+
+            count++;
+
+            if(sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i-1]){
+
+                return false;
+            }
+        }
+    }
+    return count <= 1;
+}
