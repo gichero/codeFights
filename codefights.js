d5387e9d60e3088ec8406a59d7b1306b0619d877
Amos <amosgathungu@gmail.com>
2017-07-28 00:28:33 -0400

    algorithm practice

--- codefights.js
+++ codefights.js
@@ -27,3 +27,29 @@ function shapeArea(n) {
     }
     return shapeArea;
 }
+
+ //Make Array consecutive 2
+ //Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+// Example
+//
+// For statues = [6, 2, 3, 8], the output should be
+// makeArrayConsecutive2(statues) = 3.
+//
+// Ratiorg needs statues of sizes 4, 5 and 7.
+
+ function makeArrayConsecutive2(statues) {
+
+    statues.sort((a, b)=>{
+        return a-b;
+    })
+
+    var min = statues[0];
+    var max =statues[statues.length-1];
+    count = 0;
+    for(var i=min; i<max; i++){
+        if (statues.indexOf(i) === -1){
+            count++;
+        }
+    }
+    return count;
