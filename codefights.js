a7c8f0dee711c069f366189bd40c60b0b8ce7186
Amos <amosgathungu@gmail.com>
2017-08-01 23:37:14 -0400

    algorithm practice

--- codefights.js
+++ codefights.js
@@ -137,8 +137,50 @@ function allLongestStrings(inputArray) {
 
          }
    }
+   //filter the items that are less that longestLength
    inputArray = inputArray.filter((item)=>{
       return item.length === longestLength;
    });
    return inputArray;
+}
+
+//commonCharacterCount
+//Given two strings, find the number of common characters between them.
+// Example
+// For s1 = "aabcc" and s2 = "adcaa", the output should be
+// commonCharacterCount(s1, s2) = 3.
+// Strings have 3 common characters - 2 "a"s and 1 "c".
+function commonCharacterCount(s1, s2) {
+    s1 = s1.split("");
+    s2 = s2.split("");
+
+    let s1Object = {};
+    let s2Object = {};
+
+    for (let i = 0; i < s1.length; i++){
+        if(s1Object.hasOwnProperty(s1[i]) === false){
+           s1Object[s1[i]] = 1;
+           }else{
+               s1Object[s1[i]]++;
+           }
+    }
+    for (let i = 0; i < s2.length; i++){
+        if(s2Object.hasOwnProperty(s2[i]) === false){
+           s2Object[s2[i]] = 1;
+           }else{
+               s2Object[s2[i]]++;
+           }
+    }
+    let sum  = 0;
+
+    for(let prop in s1Object){
+        if(s2Object.hasOwnProperty(prop) === true){
+           if(s2Object[prop] < s1Object[prop]){
+              sum += s2Object[prop];
+              }else{
+                  sum += s1Object[prop];
+              }
+           }
+    }
+    return sum;
 }
