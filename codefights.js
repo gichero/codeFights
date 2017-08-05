2ec022ff97cec76f866548fef2ed4fc2eaefc3e9
Amos <amosgathungu@gmail.com>
2017-08-04 20:41:23 -0400

    algorithm practice (incomplete)

--- codefights.js
+++ codefights.js
@@ -134,7 +134,6 @@ function allLongestStrings(inputArray) {
    for(var i = 0; i<inputArray.length; i++){
       if(longestLength < inputArray[i].length){
          longestLength = inputArray[i].length;
-
          }
    }
    //filter the items that are less that longestLength
@@ -238,3 +237,10 @@ function sortByHeight(a) {
     }
    return a;
 }
+
+//reverseParentheses
+// You have a string s that consists of English letters, punctuation marks, whitespace characters, and brackets. It is guaranteed that the parentheses in s form a regular bracket sequence.
+// Your task is to reverse the strings contained in each pair of matching parentheses, starting from the innermost pair. The results string should not contain any parentheses.
+// Example
+// For string s = "a(bc)de", the output should be
+// reverseParentheses(s) = "acbde".
