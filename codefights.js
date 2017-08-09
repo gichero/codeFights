fa3374726fe5d653e49dc717e43b259b044071c2
Amos <amosgathungu@gmail.com>
2017-08-08 23:48:05 -0400

    algorithm practice

--- codefights.js
+++ codefights.js
@@ -330,3 +330,43 @@ function addBorder(picture) {
 
     return picture.map(v=>'*'+v+'*')
 }
+//areSimilar
+// Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
+// Given two arrays a and b, check whether they are similar.
+// Example
+// For a = [1, 2, 3] and b = [1, 2, 3], the output should be
+// areSimilar(a, b) = true.
+// The arrays are equal, no need to swap any elements.
+// For a = [1, 2, 3] and b = [2, 1, 3], the output should be
+// areSimilar(a, b) = true.
+// We can obtain b from a by swapping 2 and 1 in b.
+// For a = [1, 2, 2] and b = [2, 1, 1], the output should be
+// areSimilar(a, b) = false.
+// Any swap of any two elements either in a or in b won't make a and b equal.
+function areSimilar(a, b) {
+
+  if(a.toString() === b.toString() ){
+       return true;
+       }
+
+
+   var arr1 = [];
+   var arr2 = [];
+
+    for (i = 0; i<a.length; i++){
+        if(a[i] !== b[i]){
+            arr1.push(a[i]);
+            arr2.push(b[i]);
+           }
+    }
+    arr2 = arr2.reverse();
+
+    if(arr1.length === 2 && (arr1.toString() === arr2.toString())){
+        return true;
+    }else{
+        return false;
+    }
+
+
+}
+//solution b
