55c74f3a95b3d7c402808b660332d800d6402f58
Amos <amosgathungu@gmail.com>
2017-09-02 18:53:30 -0400

    algorithm practice

--- codefights.js
+++ codefights.js
@@ -597,3 +597,25 @@ function isIPv4Address(inputString) {
     }
     return false;
 }
+
+//avoidObstacles
+// You are given an array of integers representing coordinates of obstacles situated on a straight line.
+// Assume that you are jumping from the point with coordinate 0 to the right. You are allowed only to make jumps of the same length represented by some integer.
+// Find the minimal length of the jump enough to avoid all the obstacles.
+// Example
+// For inputArray = [5, 3, 6, 7, 9], the output should be
+// avoidObstacles(inputArray) = 4.
+function avoidObstacles(inputArray) {
+    inputArray = inputArray.sort((a,b) =>{
+       return a - b;
+    });
+
+    for(var i = 1; i <= inputArray[inputArray.length-1] + 1; i++){
+        if(inputArray.every((item)=>{
+            return item % i !=0;
+        })){
+           return i;
+           }
+    }
+
+}
