9ffdfb2b6d621ad09ae2719f5f6d4ea83c4fa5ac
Amos <amosgathungu@gmail.com>
2017-09-16 21:15:17 -0400

    algorithm practice

--- codefights.js
+++ codefights.js
@@ -742,3 +742,19 @@ for(var i = 0; i < array1.length; i++){
  return array3;
 }
 zip([1,2,3],['a', 'b', 'c']);
+
+//arrayReplace
+// Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+// Example
+// For inputArray = [1, 2, 1], elemToReplace = 1 and substitutionElem = 3, the output should be
+// arrayReplace(inputArray, elemToReplace, substitutionElem) = [3, 2, 3].
+function arrayReplace(inputArray, elemToReplace, substitutionElem) {
+
+    inputArray.forEach(function(item, index){
+        if(item === elemToReplace){
+            inputArray[index] = substitutionElem;
+        }
+
+    });
+    return inputArray;
+}
