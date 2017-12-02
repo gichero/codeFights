d71cf8a78d9e2c08ada88a6de72035918cf92f2b
Amos <amosgathungu@gmail.com>
2017-12-01 23:32:44 -0500

    algorithm challenge

--- codefights3.js
+++ codefights3.js
@@ -6,3 +6,25 @@
 //
 // For n = 10 and firstNumber = 2, the output should be
 // circleOfNumbers(n, firstNumber) = 7.
+
+function circleOfNumbers(n, firstNumber) {
+let half = n/2;
+let result = firstNumber + half;
+    if(result > (n-1)){
+        result = result - n;
+    }
+    return result;
+}
+
+//solution b
+function circleOfNumbers(n, firstNumber) {
+    var arr = [];
+    for ( var i = 0; i < n; i++ ) {
+        arr.push(i);
+    }
+    if ( arr.indexOf(firstNumber) < arr.length /2 ) {
+        return arr.indexOf(firstNumber) + arr.length / 2;
+    } else {
+        return arr.indexOf(firstNumber) - arr.length / 2;
+    }
+}
